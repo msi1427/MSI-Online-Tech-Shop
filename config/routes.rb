34241da3d6d200bb9ad2e7389e7dcd3a5228aa1b
msi1427 <sabikirbaz@iut-dhaka.edu>
2019-12-02 17:59:19 +0600

    form creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items do
     member do
       delete :delete
@@ -7,6 +8,8 @@ Rails.application.routes.draw do
   resources :carts
   root 'store#index', as: 'store_index'
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
